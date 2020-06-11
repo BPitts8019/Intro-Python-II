b02487561ec0d96a3c152de92768553c87afe5a0
BPitts8019 <bradleyj.pitts@gmail.com>
2020-06-11 16:31:53 -0700

    chore: Reorganize game_objects for maintainability

--- src/game_objects.py
+++ src/game_objects.py
@@ -1,20 +1,46 @@
 from room import Room
 from item import Item
 
-# Declare all the rooms
-outside = Room(
-    "Outside Cave Entrance",
-    "North of you, the cave mouth beckons",
-    Item(
+# Declare all the Items
+items = {
+    "torch": Item(
         "Torch",
         "A rustic wooden handle wrapped in silk.",
         "torch"
     ),
-    Item(
+    "whip": Item(
         "Whip",
         "A long, strong, cord made of leather. It's perfect for swinging across gaps.",
         "whip"
+    ),
+    "gold": Item(
+        "Gold Ore",
+        "Unrefined, but still worth the time and effort to get.",
+        "gold", "gold ore", "ore"
+    ),
+    "pickaxe": Item(
+        "Pickaxe",
+        "A heafty tool used for mining ore.",
+        "pick", "pickaxe"
+    ),
+    "item5": Item(
+        "Item 05",
+        "some item",
+        "item5"
+    ),
+    "item6": Item(
+        "Item 06",
+        "maybe it's nothing",
+        "item6"
     )
+}
+
+# Declare all the rooms
+outside = Room(
+    "Outside Cave Entrance",
+    "North of you, the cave mouth beckons",
+    items["torch"],
+    items["whip"]
 )
 
 foyer = Room(
@@ -31,32 +57,16 @@ a light flickers in the distance, but there is no way across the chasm."""
 narrow = Room(
     "Narrow Passage",
     "The narrow passage bends here from west to north. The smell of gold permeates the air.",
-    Item(
-        "Gold Ore",
-        "Unrefined, but still worth the time and effort to get.",
-        "gold", "gold ore", "ore"
-    )
+    items["gold"]
 )
 
 treasure = Room(
     "Treasure Chamber",
     """You've found the long-lost treasure chamber! Sadly, it has already been completely 
 emptied by earlier adventurers. The only exit is to the south.""",
-    Item(
-        "Pickaxe",
-        "A heafty tool used for mining ore.",
-        "pick", "pickaxe"
-    ),
-    Item(
-        "Item 05",
-        "some item",
-        "item5"
-    ),
-    Item(
-        "Item 06",
-        "maybe it's nothing",
-        "item6"
-    )
+    items["pickaxe"],
+    items["item5"],
+    items["item6"]
 )
 
 # Link rooms together
